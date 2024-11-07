6ad6031667d2bbe2d569161d9db0534fa4a49c92
Marwan <marwanghandour@zohomai.com>
2024-11-07 18:54:41 +0200

    firstcommit

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,14 +1,13 @@
+import 'package:car_app/presentation/widgets/main_components/home_view_widgets.dart';
 import 'package:flutter/material.dart';
-import 'product/product_list.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const ProductList(),
+    return const Scaffold(
+      body: HomeViewWidgets(),
     );
   }
 }
---
+++ lib/presentation/widgets/main_components/home_view_widgets.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class HomeViewWidgets extends StatelessWidget {
+  const HomeViewWidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
